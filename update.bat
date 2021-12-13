523f4a419e8d7570d921e2e82332edd749493323
LioranWaters <96011866+LioranWaters@users.noreply.github.com>
2021-12-12 21:01:51 -0500

    1.99.8

--- update.bat
+++ update.bat
@@ -1,9 +1,13 @@
 Taskkill /im "LioranBoard 2.0.exe"
-TIMEOUT /T 2 /NOBREAK
+TIMEOUT /T 1 /NOBREAK
 del "LioranBoard 2.0.exe"
 del "data.win"
 del "DLLExtension.dll"
-rename "%cd%update/lioranboard20exe.dat" "%cd%LioranBoard 2.0.exe"
-rename "%cd%update/datawin.dat" "%cd%data.win"
-rename "%cd%update/dllextensiondll.dat" "%cd%DLLExtension.dll"
+rename "%cd%\update\lioranboard20exe.dat" "LioranBoard 2.0.exe"
+rename "%cd%\update\datawin.dat" "data.win"
+rename "%cd%\update\dllextensiondll.dat" "DLLExtension.dll"
+move "%cd%\update\LioranBoard 2.0.exe" "%cd%\"
+move "%cd%\update\data.win" "%cd%\"
+move "%cd%\update\dllextension.dll" "%cd%\"
 start "LioranBoard 2.0.exe"
+close
